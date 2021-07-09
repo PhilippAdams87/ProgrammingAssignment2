dd4345ea0d8297be38755ac1490886a5a0c70e59
PhilippAdams87 <Philipp.Adams@gmx.de>
2021-07-09 10:20:58 +0200

    Assignment week 3 Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix
 
-## Write a short comment describing this function
-
+## The first function creates a special matrix object to  its inverse
+## loading the MASS package to make use of its inverse function for matrices
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+      x <<- y
+      inv <<- NULL
+  }
+  get <- function () {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## Cache function 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <-  x$getInverse()
+  if(!is.null(inv)) {
+    message("Retrieving cached data!")
+    return(inv)
+  }
+  mat <- x$get
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
 }
